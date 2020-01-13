36f54c5b544fb6a0702ce21a68c37ecef5b31d7d
fesave <fernando.sanz.velasco@gmail.com>
2020-01-13 16:17:37 +0100

    S06-CL12

--- app/src/main/java/com/machucapps/tictactoe/ui/FindGameActivity.java
+++ app/src/main/java/com/machucapps/tictactoe/ui/FindGameActivity.java
@@ -3,14 +3,59 @@ package com.machucapps.tictactoe.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.machucapps.tictactoe.R;
+import com.machucapps.tictactoe.base.BaseActivity;
 
-public class FindGameActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+/**
+ * FindGameActivity Class
+ */
+public class FindGameActivity extends BaseActivity {
+
+    /**
+     * BindView's
+     */
+    @BindView(R.id.textViewLoading)
+    TextView mTxtLoading;
+
+    @BindView(R.id.progressBarGames)
+    ProgressBar mPbGames;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return
+     */
+    @Override
+    public int getLayoutID() {
+        return R.layout.activity_find_game;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_game);
+        mPbGames.setIndeterminate(true);
+        mTxtLoading.setText("Cargando...");
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param firebaseUser - Firebase User
+     * @param isSignUp
+     */
+    @Override
+    public void updateUI(FirebaseUser firebaseUser, Boolean isSignUp) {
+
     }
 }
